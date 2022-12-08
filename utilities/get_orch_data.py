a9dadd8f618eb014d7df15a3582db78ae42011bf
Mike Rae <mikerae@me.com>
2022-12-08 10:08:47 +0000

    Refactor get_orch_data.py

--- utilities/get_orch_data.py
+++ utilities/get_orch_data.py
@@ -1,34 +1,47 @@
 """
-Get list of file paths for RSNO schedule.csv files. These schedules are
-output from the RSNO office in csv format and are dirty.
-The utility requires that the current working directory is set in the terminal
-to where the files are located.
-In this version, there is only one file.
-The full file path is returned of the schedule.csv is returned.
+Get raw data for RSNO Strings Player List (with Annual Free Day Allocation),
+and sample Project Data for four weeks.
+
 """
 import gspread
 from google.oauth2.service_account import Credentials
-import pandas as pd
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
 
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open("string_rota_data")
+def connect_to_worksheet():
+    """
+    Coonect to Google Drive Worksheet conntaining
+    sample data for 4 weeks of projects, and string player
+    Annual Natural Free Day Allocation.
 
-projects = SHEET.worksheet('projects')
-projects_raw_data = projects.get_all_records()
+    """
+    SCOPE = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive.file",
+        "https://www.googleapis.com/auth/drive"
+        ]
 
-players = SHEET.worksheet('players')
-players_raw_data = players.get_all_records()
+    CREDS = Credentials.from_service_account_file('creds.json')
+    SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+    GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+    SHEET = GSPREAD_CLIENT.open("string_rota_data")
 
-projects_landing_df = pd.DataFrame(projects_raw_data)
-players_landing_df = pd.DataFrame(players.get_all_records())
+    return SHEET
 
-print(projects_landing_df.head())
-print(players_landing_df.head())
+
+def get_projects(SHEET):
+    """
+    Import raw sample data for 4 weeks of RSNO projects
+    """
+
+    projects = SHEET.worksheet('projects')
+    return projects.get_all_records()
+
+
+def get_players(SHEET):
+    """
+    Import raw sample data for RSNO String Player
+    Annual Natural Free Day Allocation
+    """
+
+    players = SHEET.worksheet('players')
+    return players.get_all_records()
