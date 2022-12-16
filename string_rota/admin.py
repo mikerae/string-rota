ce8f3ec654d2e2c36f17832eed310dd2c8e29660
Mike Rae <mikerae@me.com>
2022-12-16 17:45:12 +0000

    Add all models - see detail
    All models are coded, but only 2 non-dependant models are not commented
    out.
    Initial test data for the Repertoire model was deleted.
    Values for the Section model were input manually.
    
    This is an attempt to avoid ForeignKey errors as dependant data is added
    to the database.
    
    Next up: manually adding Repertoire data.
    	modified:   assets/images/string-rota-erd-schema.png
    	modified:   string_rota/admin.py
    	new file:   string_rota/migrations/0006_auto_20221216_1740.py
    	new file:   string_rota/migrations/0007_alter_section_section_name.py
    	modified:   string_rota/models.py

--- string_rota/admin.py
+++ string_rota/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
-from .models import Project, Repertoire, ProjectRepertoire
+from .models import (
+    Repertoire,
+    Section,
+    # Player,
+    # Role,
+    # Session,
+    # Project,
+    # Seating_Position,
+    # Seating_Plan,
+    # Player_Project
+)
+
 
 # Register your models here.
-admin.site.register(Project)
 admin.site.register(Repertoire)
-admin.site.register(ProjectRepertoire)
+admin.site.register(Section)
+# admin.site.register(Player)
+# admin.site.register(Role)
+# admin.site.register(Session)
+# admin.site.register(Project)
+# admin.site.register(Seating_Position)
+# admin.site.register(Seating_Plan)
+# admin.site.register(Player_Project)
--- string_rota/models.py
+++ string_rota/models.py
@@ -1,12 +1,5 @@
 from django.db import models
-
-
-# Create your models here.
-class Project(models.Model):
-    project_name = models.CharField(max_length=200, null=False, blank=False)
-
-    def __str__(self):
-        return self.project_name
+from django.conf import settings
 
 
 class Repertoire(models.Model):
@@ -15,97 +8,144 @@ class Repertoire(models.Model):
         )
     instrumentation = models.CharField(max_length=14, null=False, blank=False)
 
+    class Meta:
+        verbose_name_plural = "repertoire"
+
     def __str__(self):
         return self.rep_name
 
 
-class ProjectRepertoire(models.Model):
-    project = models.OneToOneField(
-        Project,
-        on_delete=models.CASCADE,
-    )
-    repertoire = models.ManyToManyField(
-        Repertoire,
-    )
+class Section(models.Model):
+    section_name = models.CharField(max_length=11, null=False, blank=False)
 
     def __str__(self):
-        return self.project.project_name
-
-# class Player_Project(models.Model):
-#     PERFORMANCE_STATUS_CHOICES = (
-#         (PL, 'Playing'),
-#         (RE, 'Reserve'),
-#         (NA, 'Not Available')
-#     )
-
-#     performance_status = models.CharField(
-#         max_lenght=2,
-#         null=False,
-#         none=False,
-#         choices=PERFORMANCE_STATUS_CHOICES,
-#         default=PL
-#         )
-#     off_reduced_rep = models.BooleanField(
-#         null=False, blank=False, default=False
-#         )
-
-#     def nfd_this_project():
-#         pass
-#     nfd_this_project = property(_nfd_this_project)
-#     trialist = models.BooleanField(null=False, blank=False, default=False)
-#     guest_principal = models.BooleanField(
-#         null=False, none=False, default=False
-#         )
+        return self.section_name
 
 
 # class Player(models.Model):
 
-#     VLN1 = 'VLN1'
-#     VLN2 = 'VLN2'
-#     VLA = 'VLA'
-#     VLC = 'VLC'
-#     DB = 'DB'
-
-#     SECTION_CHOICES = [
-#         (VLN1, 'First Violin'),
-#         (VLN2, 'Second Violin'),
-#         (VLA, 'Viola'),
-#         (VLC, 'Cello'),
-#         (DB, 'Double Bass')
-#     ]
-
-#     project = models.ManyToManyField(project, through=player_project)
-#     player_name = CharField(max_length=50, unique=True)
+#     player_name = models.CharField(max_length=50, unique=True)
 #     is_contract = models.BooleanField(
 #         null=False, blank=False, default=True
 #         )
 #     player_notes = models.TextField()
-#     annual_nfd_quota = models.IntegerField(null=False, blank=False)
-#     nfds_used_to_date = models.IntegerField()
-
-#     def _nfds_over_quota(self.annual_nfd_quota, self.nfds_used_to_date):
-#         return self.annual_nfd_quota - self.fds_used_to_date
-
-#     nfds_over_quota = property(_nfds_over_quota)
-#     off_reduced_rep_tot = models.IntegerField()
-#     section = models.CharField(
-#         max_length=4, choices=SECTION_CHOICES, null=False, blank=False
-#     )
-#     users_django_id = OneToOneField(
+#     annual_nfd_quota = models.IntegerField(
+# null=False, blank=False, default=0)
+#     nfds_used_to_date = models.IntegerField(
+# null=False, blank=False, default=0)
+#     off_reduced_rep_tot = models.IntegerField(
+#         null=False, blank=False, default=0
+#         )
+#     section_name = models.ForeignKey(Section,  on_delete=models.CASCADE)
+#     users_django_id = models.OneToOneField(
 #         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
 #         )
-#     roles_djange_id = OneToOneField(
-#         settings.AUTH_GROUPS_MODEL, on_delete=models.CASCADE
-#         )
+
+#     def __str__(self):
+#         return self.player_name
 
 
-# class Seating_Plan(models.Model):
-#     pass
+# class Role(models.Model):
+#     player = models.BooleanField(default=True)
+#     rota_manager = models.BooleanField()
+#     office = models.BooleanField()
+#     super_user = models.BooleanField()
+#     users_django_id = models.ManyToManyField(settings.AUTH_USER_MODEL)
 
-
-# class Seating_Position(models.Model):
-#     pass
+#     def __str__(self):
+#         return self.users_django_id
 
 
 # class Session(models.Model):
-#     pass
+#     SESSION_TYPE_CHOICES = [
+#         ('REH', 'Rehersal'),
+#         ('CON', 'Concert'),
+#         ('REC', 'Recording'),
+#     ]
+#     session_date = models.DateField(
+#         auto_now=False, auto_now_add=False, null=False, blank=False
+#         )
+#     session_start_time = models.DateTimeField(null=False, blank=False)
+#     session_end_time = models.DateTimeField(null=False, blank=False)
+#     session_type = models.CharField(
+#         max_length=3, choices=SESSION_TYPE_CHOICES, default='REH'
+#         )
+#     repertoire_id = models.ForeignKey(Repertoire, on_delete=models.RESTRICT)
+
+#     def __str__(self):
+#         return self.session_type
+
+
+# class Project(models.Model):
+#     project_name = models.CharField(max_length=200, null=False, blank=False)
+#     player = models.ManyToManyField(Player, through='player_project')
+#     repertoire_name = models.ManyToManyField(Repertoire)
+#     seating_plan = models.ManyToManyField(
+#             'Seating_Position', through='Seating_Plan'
+#         )
+#     session = models.ForeignKey(Session, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.project_name
+
+
+# class Seating_Position(models.Model):
+#     position_number = models.IntegerField()
+#     seating_plan = models.ForeignKey(
+#         'Seating_Plan', on_delete=models.CASCADE, related_name='plan',
+#         )
+#     player_id = models.ForeignKey(
+#         Player, on_delete=models.CASCADE
+#         )
+#     section_id = models.ForeignKey(
+#         Section, on_delete=models.CASCADE
+#         )
+
+#     def __str__(self):
+#         return self.position_number
+
+
+# class Seating_Plan(models.Model):
+#     is_published = models.BooleanField(default=False)
+#     project_id = models.ForeignKey(
+#         Project, on_delete=models.CASCADE, related_name='seating_plans',
+#         )
+#     section_name = models.ForeignKey(Section, on_delete=models.CASCADE)
+#     seating_position = models.ForeignKey(
+#         Seating_Position, on_delete=models.CASCADE, related_name='postition',
+#         )
+#     player = models.ManyToManyField(Player, through='seating_position',)
+
+#     def __str__(self):
+#         return self.project_id
+
+
+# class Player_Project(models.Model):
+#     PERFORMANCE_STATUS_CHOICES = [
+#         ('PL', 'Playing'),
+#         ('RE', 'Reserve'),
+#         ('NA', 'Not Available'),
+#     ]
+#     performance_status = models.CharField(
+#         max_length=2,
+#         null=False,
+#         blank=False,
+#         choices=PERFORMANCE_STATUS_CHOICES,
+#         default='PL'
+#         )
+#     off_reduced_rep = models.BooleanField(
+#         null=False, default=False
+#         )
+#     trialist = models.BooleanField(null=False, blank=False, default=False)
+#     guest_principal = models.BooleanField(
+#         null=False, default=False
+#         )
+#     project_id = models.ForeignKey(
+#         Project,  on_delete=models.CASCADE
+#         )
+#     player_id = models.ForeignKey(
+#         Player,  on_delete=models.CASCADE
+#         )
+
+#     def __str__(self):
+#         return self.performance_status
