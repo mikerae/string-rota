2dc45ae370b3036ed1365e723e82dd838685b16b
Mike Rae <mikerae@me.com>
2022-12-26 17:42:13 +0000

    Remove commented out code in views.py

--- string_rota/views.py
+++ string_rota/views.py
@@ -20,37 +20,6 @@ class Projects(View):
 
     def get(self, request):
         projects = Project.objects.all()
-        # project = projects.first()
-        # sections = Section.objects.all()
-        # section = get_object_or_404(Section.objects.all(), name='Violin 1')
-        # players_in_project = Player_Project.objects.filter(
-        #     project=project
-        #     ).filter(player__section=section)
-        # queryset = Seating_Plan.objects.filter(
-        #     project=project,
-        #     )
-        # seating_plan = get_object_or_404(queryset, section=section.id)
-        # seating_positions = Seating_Position.objects.filter(
-        #     seating_plan=seating_plan
-        #     )
-        # res_ply = players_in_project.filter(
-        #     performance_status='RE'
-        #     )
-        # if not res_ply:
-        #     reserve_player = "Not Allocated"
-        # else:
-        #     reserve_player = res_ply.get().player
-
-        # red_ply = players_in_project.filter(
-        #     off_reduced_rep=True
-        #     )
-        # if not red_ply:
-        #     off_reduced = "Not Allocated"
-        # else:
-        #     off_reduced = red_ply.get().player
-        # not_available = players_in_project.filter(performance_status='NA')
-        # print(off_reduced)
-        # repertoire = project.repertoire_name.all()
 
         context = {
             'projects': projects,
@@ -91,7 +60,6 @@ class Rota(Projects):
         else:
             off_reduced = red_ply.get().player
         not_available = players_in_project.filter(performance_status='NA')
-        print(off_reduced)
         repertoire = project.repertoire_name.all()
 
         context = {
