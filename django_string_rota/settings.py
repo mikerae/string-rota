dccff07e1a661bb1e9ee61f11b730231532f9190
Mike Rae <mikerae@me.com>
2023-07-28 15:41:43 +0000

    Set DEBUG to false

--- django_string_rota/settings.py
+++ django_string_rota/settings.py
@@ -27,8 +27,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-mikerae-stringrota-srhpnofzijg.ws-eu102.gitpod.io',
