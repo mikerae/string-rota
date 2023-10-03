dc67945d4b40b2041886aa89c0ad54dff89819c3
Mike Rae <mikerae@me.com>
2023-10-03 21:28:19 +0000

    [#138] Add register user functionality
    POST method of register class recieves:
    - the selected player to register id,
    - username
    - password1
    - password2
    It checkes the passwords are the same, then sets valid to true
    and creates a new user.
    The new user is given the first and last name of the Player record.
    and added to the group 'Player'
    The Player.users_django field is linked to the user
    
    Success and advisory messeges are passed to the redirect page.
    	modified:   player_info/views.py
    
    Add UserCreationForm to register.html
    	modified:   player_info/templates/player_info/register.html

--- player_info/views.py
+++ player_info/views.py
@@ -8,6 +8,8 @@ from django.shortcuts import get_object_or_404, reverse
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 
 from string_rota.models import Project, Player
 from string_rota.utilities import (
@@ -44,7 +46,6 @@ class Register(View):
         rota_manager = request.user.groups.filter(name="Rota_Manager")
         projects = Project.objects.all()
         section_players = get_players(section)
-
         unregistered_players = section_players.filter(users_django__isnull=True)  # noqa
 
         create_user_form = UserCreationForm()
@@ -69,11 +70,53 @@ class Register(View):
         """Create user and link user to Player record"""
 
         player_id = request.POST["player_id"]
+        player = get_player(request)
+        section = get_section(player)
+        office = request.user.groups.filter(name="Office")
+        rota_manager = request.user.groups.filter(name="Rota_Manager")
+        projects = Project.objects.all()
         registered_player = get_object_or_404(Player, id=player_id)
+        section_players = get_players(section)
+        unregistered_players = section_players.filter(users_django__isnull=True)  # noqa
 
-        messages.success(request, f"{registered_player} has been registered ")
-        return HttpResponseRedirect(
-            reverse(
-                "home",
+        create_user_form = UserCreationForm(request.POST)
+        if create_user_form.is_valid():
+            username = create_user_form.cleaned_data["username"]
+            password = create_user_form.cleaned_data["password1"]
+            new_user = create_user_form.save()
+            new_user.first_name = registered_player.first_name
+            new_user.last_name = registered_player.last_name
+            group = Group.objects.get(name="Player")
+            new_user.groups.add(group)
+            new_user.save()
+
+            # Link new user to Player record
+            registered_player.users_django = new_user
+            registered_player.save()
+
+            messages.success(
+                request, f"{registered_player} has been registered"
+            )  # noqa
+            messages.warning(
+                request,
+                f"Please make a note of {registered_player}'s username: {username} and password: {password}",  # noqa
             )
-        )
+            return HttpResponseRedirect(
+                reverse(
+                    "home",
+                )
+            )
+        else:
+            print("register form not valid")
+            print(f"errors:  {create_user_form.error_messages}")
+            template = "player_info/register.html"
+            context = {
+                "section": section,
+                "office": office,
+                "rota_manager": rota_manager,
+                "projects": projects,
+                "unregistered_players": unregistered_players,
+                "create_user_form": create_user_form,
+                "create_user_form_errors": create_user_form.error_messages,
+            }
+            return render(request, template, context)
