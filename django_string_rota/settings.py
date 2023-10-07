86ac97eb262f97c1c7f0f22326dce6448f92d792
Mike Rae <mikerae@me.com>
2023-10-07 14:05:43 +0000

    [#166] Add gmail settings for email server
    	modified:   django_string_rota/settings.py

--- django_string_rota/settings.py
+++ django_string_rota/settings.py
@@ -120,9 +120,6 @@ else:
 
 ACCOUNT_EMAIL_VERIFICATION = "none"
 
-# Email Server
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -175,3 +172,17 @@ DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Email Server
+
+if "DEVELOPMENT" in os.environ:
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    DEFAULT_FROM_EMAIL = "info@example.com"
+else:
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = "smtp.gmail.com"
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
+    DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
